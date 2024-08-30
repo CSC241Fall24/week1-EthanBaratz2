f01cf90dba180ee4a7d167c9e1241d1eb546097d
EthanBaratz2 <157398612+EthanBaratz2@users.noreply.github.com>
2024-08-30 11:19:35 -0400

    Update Bulbasaur.java

--- ex2/src/main/java/Bulbasaur.java
+++ ex2/src/main/java/Bulbasaur.java
@@ -2,31 +2,22 @@ public class Bulbasaur {
     private int id = 1;
     private int level = 1;
 
-    // Constructor
     public Bulbasaur() {
         id = 1;
         level = 1;
     }
 
-    // setLevel method
     public void setLevel(int lv) {
-        // TODO: Implement this method
-        // Update the level
-        // If the new level is 16 or higher but less than 32, evolve to Ivysaur
-        // If the new level is 32 or higher, evolve to Venusaur
-
     level = lv;
         if(level > 15 && level < 32) id = 2;
-        else if(level > 32) id = 3;
+        else if(level >= 32) id = 3;
         
     }
 
-    // getLevel method
     public int getLevel() {
         return level; // Placeholder return value
     }
 
-    // getName method
     public String getName() {
        if(id == 1) return "Bulbasaur";
     else if(id == 2) return "Ivysaur";
@@ -38,23 +29,17 @@ public class Bulbasaur {
     public int getID() {
       return id;
     }
-
-    // toString method
-    @Override
+    
     public String toString() {
-        
         return "Level: " + level + ", ID: " + id; // Placeholder return value
     }
 
-    // equals method
-    @Override
     public boolean equals(Bulbasaur obj) {
         if(obj.getLevel() != this.getLevel()) return false;
         else if(obj.getName().equals(this.getName())) return true;
         else return false;
     }
-
-    // copy method
+    
     public Bulbasaur copy() {
         // TODO: Implement this method
         // Create and return a new Bulbasaur object with the same id and level
